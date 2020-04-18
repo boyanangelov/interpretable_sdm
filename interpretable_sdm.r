145416d984450269821f265b121d267387ffd46b
Boyan Angelov <boyan.angelov@gmail.com>
2020-04-18 12:07:56 +0200

    Add variable importance, AUC calculation, normalized raster names fix

--- interpretable_sdm.r
+++ interpretable_sdm.r
@@ -3,6 +3,9 @@ library(mlr)
 library(lime)
 library(dplyr)
 library(rsample)
+library(latticeExtra)
+library(sp)
+library(ggplot2)
 
 # Get and prepare data ----------------------------------------------------
 
@@ -11,51 +14,52 @@ occ_data_raw <-
 
 # rename bioclimatic variables to be more descriptive
 names(occ_data_raw$raster_data$climate_variables) <- c(
-  "mean_temp",
-  "mean_diurnal",
-  "isotherm",
-  "seas_temp",
-  "warmest_month",
-  "coldest_month",
-  "range_temp",
-  "wettest",
-  "driest",
-  "warmest_quart",
-  "coldest_quart",
-  "precip",
-  "precip_wettest_month",
-  "precip_driest_month",
-  "precip_season",
-  "precip_wettest_quart",
-  "precip_driest_quart",
-  "precip_warmest_quart",
-  "precip_coldest_quart"
+  "Annual_Mean_Temperature",
+  "Mean_Diurnal_Range",
+  "Isothermality",
+  "Temperature_Seasonality",
+  "Max_Temperature_of_Warmest_Month",
+  "Min_Temperature_of_Coldest_Month",
+  "Temperature_Annual_Range",
+  "Mean_Temperature_of_Wettest_Quarter",
+  "Mean_Temperature_of_Driest_Quarter",
+  "Mean_Temperature_of_Warmest_Quarter",
+  "Mean_Temperature_of_Coldest_Quarter",
+  "Annual_Precipitation",
+  "Precipitation_of_Wettest_Month",
+  "Precipitation_of_Driest_Month",
+  "Precipitation_Seasonality",
+  "Precipitation_of_Wettest_Quarter",
+  "Precipitation_of_Driest_Quarter",
+  "Precipitation_of_Warmest_Quarter",
+  "Precipitation_of_Coldest_Quarter"
 )
 
 names(occ_data_raw$df_data) <- c(
-  "mean_temp",
-  "mean_diurnal",
-  "isotherm",
-  "seas_temp",
-  "warmest_month",
-  "coldest_month",
-  "range_temp",
-  "wettest",
-  "driest",
-  "warmest_quart",
-  "coldest_quart",
-  "precip",
-  "precip_wettest_month",
-  "precip_driest_month",
-  "precip_season",
-  "precip_wettest_quart",
-  "precip_driest_quart",
-  "precip_warmest_quart",
-  "precip_coldest_quart",
+  "Annual_Mean_Temperature",
+  "Mean_Diurnal_Range",
+  "Isothermality",
+  "Temperature_Seasonality",
+  "Max_Temperature_of_Warmest_Month",
+  "Min_Temperature_of_Coldest_Month",
+  "Temperature_Annual_Range",
+  "Mean_Temperature_of_Wettest_Quarter",
+  "Mean_Temperature_of_Driest_Quarter",
+  "Mean_Temperature_of_Warmest_Quarter",
+  "Mean_Temperature_of_Coldest_Quarter",
+  "Annual_Precipitation",
+  "Precipitation_of_Wettest_Month",
+  "Precipitation_of_Driest_Month",
+  "Precipitation_Seasonality",
+  "Precipitation_of_Wettest_Quarter",
+  "Precipitation_of_Driest_Quarter",
+  "Precipitation_of_Warmest_Quarter",
+  "Precipitation_of_Coldest_Quarter",
   "label"
 )
 
 occ_data <- occ_data_raw$df_data
+
 occ_data$label <- as.factor(occ_data$label)
 
 coordinates_df <- rbind(occ_data_raw$raster_data$coords_presence,
@@ -76,10 +80,6 @@ data_test  <- testing(train_test_split)
 data_train$x <- NULL
 data_train$y <- NULL
 data_test_subset <- data_test %>% filter(label == 1)
-sample_data <- withr::with_seed(44, sample_n(data_test_subset, 3))
-sample_data_coords <- dplyr::select(sample_data, c("x", "y"))
-sample_data$x <- NULL
-sample_data$y <- NULL
 
 # Train model -------------------------------------------------------------
 
@@ -87,8 +87,24 @@ task <-
   makeClassifTask(id = "model", data = data_train, target = "label")
 lrn <- makeLearner("classif.randomForest", predict.type = "prob")
 mod <- train(lrn, task)
+pred <- predict(mod, newdata=data_test)
+VIMP <- as.data.frame(getFeatureImportance(mod)$res)
+
+# Top n important variables
+top_n(VIMP, n=5, importance) %>%
+  ggplot(., aes(x=reorder(variable,importance), y=importance))+
+  geom_bar(stat='identity')+ coord_flip() + xlab("")
+
+# Performance
+performance(pred, measures=auc)
 
 # Generate explanations ---------------------------------------------------
+# resampling
+sample_data <- withr::with_seed(1, sample_n(data_test_subset, 3))
+sample_data_coords <- dplyr::select(sample_data, c("x", "y"))
+sample_data$x <- NULL
+sample_data$y <- NULL
+
 
 set.seed(42)
 explainer <- lime(data_train, mod)
@@ -107,11 +123,18 @@ customPredictFun <- function(model, data) {
   return(v$presence)
 }
 
+normalized_raster <- RStoolbox::normImage(occ_data_raw$raster_data$climate_variables)
+names(normalized_raster) <- names(occ_data_raw$raster_data$climate_variables)
 pr <-
-  dismo::predict(RStoolbox::normImage(occ_data_raw$raster_data$climate_variables),
+  dismo::predict(normalized_raster,
                  mlr::getLearnerModel(mod, TRUE),
                  fun = customPredictFun)
+
+
+coordinates(sample_data_coords) <- ~ x + y
 rf_map <-
-  raster::spplot(pr, main = "Predicted Habitat Suitability Map (Random Forest)", ylab = "Suitability")
+  spplot(pr, main = "Habitat Suitability Map", 
+         scales = list(draw = TRUE),
+         sp.layout = list("sp.points", sample_data_coords, pch=1, cex=1.2, lwd=2, col="white"))
 rf_map
 
